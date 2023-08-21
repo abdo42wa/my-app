13bce8a5852b79b56e0994a931bf0a838d714575
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2023-08-21 18:28:57 +0300

    added animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React from 'react';
+import ScrollContainer from 'react-scroll-horizontal';
+import './images/1.jpg'
+
 import './App.css';
-import Page from './components/Page';
+
+const pages = [
+  {
+    title: 'Page 1',
+    imageUrl: 'https://img.freepik.com/free-photo/dramatic-white-clouds-blue-sky-from-airplane-window-view-colorful-sunset-cloudscape-background_90220-1208.jpg',
+    text: 'Welcome to Page 1',
+  },
+  {
+    title: 'Page 2',
+    imageUrl: 'https://cdn.shopify.com/s/files/1/0070/7032/files/best-free-stock-photo-websites.jpg?v=1619036042',
+    text: 'Welcome to Page 2',
+  },
+  {
+    title: 'Page 3',
+    imageUrl: 'https://img.freepik.com/free-photo/digital-painting-mountain-with-colorful-tree-foreground_1340-25699.jpg',
+    text: 'Welcome to Page 3',
+  },
+  {
+    title: 'Page 4',
+    imageUrl: 'https://images.pexels.com/photos/296282/pexels-photo-296282.jpeg?cs=srgb&dl=pexels-lukas-296282.jpg&fm=jpg',
+    text: 'Welcome to Page 3',
+  },
+  {
+    title: 'Page 5',
+    imageUrl: 'https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?w=2000',
+    text: 'Welcome to Page 3',
+  },
+];
 
 function App() {
   return (
     <div className="App">
-
-      <Page />
-
+      <ScrollContainer>
+        {pages.map((page, index) => (
+          <div key={index} className="page">
+            <img src={page.imageUrl} alt={`Page ${index + 1}`} />
+            <div className="page-content">
+              <h2>{page.title}</h2>
+              <p>{page.text}</p>
+            </div>
+          </div>
+        ))}
+      </ScrollContainer>
     </div>
   );
 }
--- src/components/Page.js
+++ src/components/Page.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import MagicScroll from 'magic-scroll';
 
 const Page = () => {
+
     return (
         <div className="page">
-            <div className="centered-text">Page 1</div>
-            <div className="centered-text">Page 2</div>
-            <div className="centered-text">Page 3</div>
-            <div className="centered-text">Page 4</div>
-            <div className="centered-text">Page 5</div>
-            <div className="centered-text">Page 6</div>
-            <div className="centered-text">Page 7</div>
+            <MagicScroll>
+                <div className="centered-text">Page 1</div>
+                <div className="centered-text">Page 2</div>
+                <div className="centered-text">Page 3</div>
+                <div className="centered-text">Page 4</div>
+                <div className="centered-text">Page 5</div>
+                <div className="centered-text">Page 6</div>
+                <div className="centered-text">Page 7</div>
 
+            </MagicScroll>
         </div>
     );
 };
